a7663fd2e641c7be4ddb3198e873d7adb9c1f59c
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-02-12 17:11:54 +0200

    chore: Remove unused code

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,14 +8,10 @@ const client = new MongoClient(url);
 export let db: Db;
 
 // Database Name
-const dbName = 'AgentForge';
 export async function connectDb() {
     // Use connect method to connect to the server
     await client.connect();
     console.log('Connected successfully to server');
-    db = client.db(dbName);
-    
-    
-    return 'done.';
+    db = client.db(env('DB_NAME'));
 }
 
