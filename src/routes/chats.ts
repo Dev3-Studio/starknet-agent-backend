1d21d2374f277746e02fd0c7e42b71fd4f8adf89
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-02-13 14:35:11 +0200

    fix: Missing export

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -7,3 +7,5 @@ router.get('/:id', controller.getChat);
 router.post('/', controller.createChat);
 router.post('/:id/messages', controller.addUserMessage);
 router.delete('/:id', controller.deleteChat);
+
+export default router;
