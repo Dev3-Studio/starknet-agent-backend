70c42859c1f02fc28f914c2a9a5bf31f35fb45ff
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-02-13 19:44:57 +0200

    improve: Agent routes

--- src/controllers/agents.ts
+++ src/controllers/agents.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import * as services from '../services/agents';
 import { zAgentCreate } from '../lib/dto';
+import { ForbiddenError } from '../lib/httpErrors';
 
 export async function getAgent(req: Request, res: Response) {
     const { id } = req.params;
     const user = req.user;
-    res.send(await services.getAgent(id, user?.address));
+    const agentCreator = await services.getAgentCreator(id);
+    res.send(await services.getAgent(id, user?.id === agentCreator.id));
 }
 
 export async function getAgents(req: Request, res: Response) {
@@ -21,12 +23,16 @@ export async function getAgents(req: Request, res: Response) {
 
 export async function createAgent(req: Request, res: Response) {
     const agent = zAgentCreate.parse(req.body);
-    res.send(await services.createAgent(agent));
+    const user = req.user;
+    if (!user?.id) throw new ForbiddenError('User not found');
+    res.send(await services.createAgent({ ...agent, creator: user.id }));
 }
 
 export async function updateAgent(req: Request, res: Response) {
     const { id } = req.params;
-    const agent = zAgentCreate.parse(req.body);
     const user = req.user;
-    res.send(await services.updateAgent(id, agent, user?.address));
+    const agent = zAgentCreate.parse(req.body);
+    const agentCreator = await services.getAgentCreator(id);
+    if (user?.id !== agentCreator.id) throw new ForbiddenError('User not allowed to update this agent');
+    res.send(await services.updateAgent(id, { ...agent, creator: user.id }));
 }
--- src/services/agents.ts
+++ src/services/agents.ts
@@ -1,12 +1,16 @@
-import { Agent, AgentCreate } from '../lib/dto';
+import { Agent, AgentCreate, AgentPublic, User, zAgentPublic } from '../lib/dto';
 import { AgentCollection, IAgentCollection, IUserCollection, UserCollection } from '../database/schema';
 import { decryptAes256gcm, encryptAes256gcm } from '../lib/aes256gcm';
 import { env } from '../lib/env';
-import { ForbiddenError, InternalServerError, NotFoundError } from '../lib/httpErrors';
+import { InternalServerError, NotFoundError } from '../lib/httpErrors';
 import Ajv from 'ajv';
 import { WithId } from 'mongodb';
+import { getUser } from './users';
 
-export async function getAgent(agentId: string, callerWalletAddress?: string): Promise<Agent> {
+export async function getAgent(agentId: string, includePrivateFields: false): Promise<AgentPublic>;
+export async function getAgent(agentId: string, includePrivateFields: true): Promise<Agent>;
+export async function getAgent(agentId: string, includePrivateFields?: boolean): Promise<AgentPublic | Agent>
+export async function getAgent(agentId: string, includePrivateFields?: boolean): Promise<AgentPublic | Agent> {
     const agentResult = await AgentCollection.findOne({ id: agentId });
     if (!agentResult) throw new NotFoundError('Agent not found');
     
@@ -14,8 +18,6 @@ export async function getAgent(agentId: string, callerWalletAddress?: string): P
     if (!creatorResult) throw new NotFoundError('Creator not found');
     
     // If caller is not the creator, hide private fields
-    const includePrivateFields = (callerWalletAddress ?? '').toLowerCase() === creatorResult.walletAddress.toLowerCase();
-    
     const formattedAgent = formatAgentForResponse(agentResult, creatorResult);
     
     return {
@@ -34,7 +36,7 @@ interface GetAgentsProps {
     order?: string;
 }
 
-export async function getAgents(props: GetAgentsProps): Promise<Agent[]> {
+export async function getAgents(props: GetAgentsProps): Promise<AgentPublic[]> {
     const { tags, limit, creator, sort, order } = props;
     const query: Record<string, unknown> = {};
     if (tags) query.tags = { $all: tags };
@@ -47,23 +49,30 @@ export async function getAgents(props: GetAgentsProps): Promise<Agent[]> {
     return agents.map(agent => {
         const creator = creators.find(c => c._id.toString() === agent.creator);
         if (!creator) throw new InternalServerError('Creator not found');
-        return formatAgentForResponse(agent, creator);
+        return zAgentPublic.parse(formatAgentForResponse(agent, creator));
     });
 }
 
-export async function createAgent(agent: AgentCreate): Promise<Agent> {
-    const res = await AgentCollection.insertOne({ ...formatAgentForInsert(agent), totalChats: 0, totalMessages: 0 });
-    return await getAgent(res.insertedId.toString());
+export async function getAgentCreator(agentId: string): Promise<User> {
+    const agentResult = await AgentCollection.findOne({ id: agentId });
+    if (!agentResult) throw new NotFoundError('Agent not found');
+    
+    return await getUser(agentResult.creator);
 }
 
-export async function updateAgent(agentId: string, agent: AgentCreate, callerWalletAddress?: string): Promise<Agent> {
-    const existingAgent = await getAgent(agentId);
-    if (existingAgent.creator.walletAddress.toLowerCase() !== callerWalletAddress?.toLowerCase()) {
-        throw new ForbiddenError('You are not the creator of this agent');
-    }
-    
+export async function createAgent(agent: AgentCreate & { creator: string }): Promise<Agent> {
+    const res = await AgentCollection.insertOne({
+        ...formatAgentForInsert(agent),
+        pricePerTokenUsd: 0, // todo add base pricing logic
+        totalChats: 0,
+        totalMessages: 0
+    });
+    return await getAgent(res.insertedId.toString(), true);
+}
+
+export async function updateAgent(agentId: string, agent: AgentCreate  & { creator: string }): Promise<Agent> {
     await AgentCollection.updateOne({ id: agentId }, { $set: formatAgentForInsert(agent) });
-    return await getAgent(agentId);
+    return await getAgent(agentId, true);
 }
 
 function validateArgumentsSchema(schema: object) {
@@ -71,7 +80,7 @@ function validateArgumentsSchema(schema: object) {
     return ajv.validateSchema(schema);
 }
 
-function formatAgentForResponse(agent: WithId<IAgentCollection>, creator: WithId<IUserCollection>): Agent {
+export function formatAgentForResponse(agent: WithId<IAgentCollection>, creator: WithId<IUserCollection>): Agent {
     return {
         ...agent,
         id: agent._id.toString(),
@@ -100,7 +109,7 @@ function formatAgentForResponse(agent: WithId<IAgentCollection>, creator: WithId
     };
 }
 
-function formatAgentForInsert(agent: AgentCreate) {
+function formatAgentForInsert(agent: AgentCreate & { creator: string }) {
     return {
         ...agent,
         tools: agent.tools.map(t => {
