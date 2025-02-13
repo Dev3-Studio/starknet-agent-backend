c5bd34c6e56b6ee78b72a2cb0f935fb4bdbb738d
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-02-13 19:55:11 +0200

    improve: Database connection logic

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,15 @@
-import { Db, MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import { env } from '../lib/env';
 
 // Connection URL
 const url = env('DB_URI');
 export const client = new MongoClient(url);
 
-export let db: Db;
+export const db = client.db(env('DB_NAME'));
 
 // Database Name
 export async function connectDb() {
     // Use connect method to connect to the server
     await client.connect();
     console.log('Connected successfully to server');
-    db = client.db(env('DB_NAME'));
 }
-
